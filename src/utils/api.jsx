e22cfb6aa438cf3e32c43cd11aecd54ebc2294df
sulirb <121161440+sulirb@users.noreply.github.com>
2023-05-29 21:21:28 +0200

    correction fetch data

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import dataUrl from "../data.json?url";
 
-export function useFetch(method, url, body, options = {}) {
+export function useFetch(method, url, body, options) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(false);
   const [isLoading, setLoading] = useState(true);
@@ -9,7 +9,7 @@ export function useFetch(method, url, body, options = {}) {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(url, { method, body, ...options });
+        const response = await fetch(url, { method, body, ...(options ?? {}) });
         const houseList = await response.json();
         setData(houseList);
       } catch (err) {
